0b02a94a52c8f19e98fefac73bb109ada84f77fe
Leticia Freitas <leticiadev24@gmail.com>
2024-12-12 20:40:24 -0300

    adding favicon

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,6 @@ module.exports = {
 
     plugins: [new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "index.html"),
+        favicon: path.resolve("src", "assets", "scissors.svg")
     })],
 }
