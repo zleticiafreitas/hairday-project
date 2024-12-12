b64b976dc176586705239193b690e8ae944a4d0d
Leticia Freitas <leticiadev24@gmail.com>
2024-12-12 20:37:21 -0300

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+    target: "web",
+    mode: "development",
+
+    entry: path.resolve(__dirname, "src", "main.js"),
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 3000,
+        open: true,
+        liveReload: true,
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "index.html"),
+    })],
+}
