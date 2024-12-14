6450c1faa4a074d83819faf6d54657c9e5ac1660
Leticia Freitas <leticiadev24@gmail.com>
2024-12-14 12:12:44 -0300

    adding current date in form

---
+++ src/modules/form/submit.js
@@ -0,0 +1,16 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.getElementById("date")
+
+// Date atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+//Carrega a data atual e define a data mínima como sendo a data atual.
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+    console.log("ENVIADO!")
+}
