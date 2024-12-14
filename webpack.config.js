09e36c10770f4ea8eedd4b782c0bd6fdd8f2232f
Leticia Freitas <leticiadev24@gmail.com>
2024-12-14 11:55:32 -0300

    finishing to set webpack, add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
 module.exports = {
     target: "web",
     mode: "development",
@@ -19,8 +21,35 @@ module.exports = {
         liveReload: true,
     },
 
-    plugins: [new HtmlWebpackPlugin({
+    plugins: [
+        new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "index.html"),
         favicon: path.resolve("src", "assets", "scissors.svg")
-    })],
+    }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "src", "assets"),
+                to: path.resolve(__dirname, "dist", "src", "assets")
+            }]
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    }
+                }
+            }
+        ]
+    }
 }
