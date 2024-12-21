25f8023a14b775c5103705f97a5b0e10b984ab1a
Leticia Freitas <leticiadev24@gmail.com>
2024-12-21 10:27:45 -0300

    configuring api and scheduling service

--- src/main.js
+++ src/main.js
@@ -10,4 +10,5 @@ import "./styles/schedule.css"
 
 // JS
 import "./modules/form/submit.js"
+import "./modules/form/date-change.js"
 import "./modules/page-load.js"
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,7 @@
 import dayjs from "dayjs"
 
+import { scheduleNew } from "../../services/schedules-new.js";
+
 const form = document.querySelector("form")
 const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
@@ -11,7 +13,7 @@ const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 selectedDate.value = inputToday
 selectedDate.min = inputToday
 
-form.onsubmit = (event) => {
+form.onsubmit = async (event) => {
     event.preventDefault()
     
     try {
@@ -35,6 +37,12 @@ form.onsubmit = (event) => {
 
         // Gera um ID
         const id = new Date().getTime()
+
+        await scheduleNew({
+            id,
+            name,
+            when,
+        })
 
     } catch (error) {
         alert("Não foi possível realizar o agendamento.")
